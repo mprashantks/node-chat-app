5fdc6e6c1f70566d5c39a9f9653826f15c70ecf5
Prashant Singh <mprashantks@gmail.com>
2017-11-13 17:12:45 +0530

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
@@ -20,10 +20,13 @@ io.on('connection', (socket) => {
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', (message, callback) => {
-    // console.log('Create message: ', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    // callback('Acknowledgement from server');
+    callback();
   });
+
+  socket.on('createLocationMessage', (message) => {
+    io.emit('newLocationMessage', generateLocationMessage(message.from, message.latitude, message.longitude));
+  })
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
